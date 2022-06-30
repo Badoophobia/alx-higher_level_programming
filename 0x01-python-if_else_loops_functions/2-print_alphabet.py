4e10a60086e53630eec99ca91898ae078e308922
Badoophobia <okyem4all@gmail.com>
2022-06-30 00:19:43 +0000

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-if number > 0:
-    value = number % 10
-elif number < 0:
-    temp = -number 
-    value = temp % 10
-var = "Last digit of"
-while value:
-    if value > 5:
-        print(f"Last digit of {number:d} is {value:d} and is greater than 5")
-    elif value == 0:
-        print(f"Last digit of {number:d} is {value:d} and is 0")
-    elif value < 6 and not 0:
-        print(f"{var} {number:d} is {value:d} and is less than 6 and not 0")
-    break
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
