c050cf9e5d9a354024b015b09610daa55ed2c2fe
Okunade Oluwayemisi <101140355+Badoophobia@users.noreply.github.com>
2022-07-27 22:02:11 +0100

    Add files via upload
    ALX commits

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+ a class Rectangle that defines a
+ rectangle by: (based on 0-rectangle.py)
+"""
+
+
+class Rectangle:
+    """
+    Private instance attribute: width
+    Private instance attribute: height
+    """
+    def __init__(self, width=0, height=0):
+        """Initializes the class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
